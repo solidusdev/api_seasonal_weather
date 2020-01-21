dca9aa12828e15edfeccc097af08980eb705cc04
Ivan <i_cuenca@aol.com>
2020-01-20 20:10:35 -0500

    Updates

---
+++ precache-manifest.3c4febf04ff18de2ad027710d3b51666.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d5609a2a1cf150b2042c5000dc0166",
+    "url": "/api_seasonal_weather/index.html"
+  },
+  {
+    "revision": "21c038a6fdc762d92647",
+    "url": "/api_seasonal_weather/static/css/main.125ef254.chunk.css"
+  },
+  {
+    "revision": "a470d3777233f51ac285",
+    "url": "/api_seasonal_weather/static/js/2.ffaf7804.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/api_seasonal_weather/static/js/2.ffaf7804.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21c038a6fdc762d92647",
+    "url": "/api_seasonal_weather/static/js/main.5a6e7343.chunk.js"
+  },
+  {
+    "revision": "41d522e3ec4b9e9e50d4",
+    "url": "/api_seasonal_weather/static/js/runtime-main.547d2bca.js"
+  },
+  {
+    "revision": "e6ee343e73a3de9c6d4e1386b0b44304",
+    "url": "/api_seasonal_weather/static/media/autumn.e6ee343e.jpg"
+  },
+  {
+    "revision": "d7c3af267b544c78f76b1e9579505756",
+    "url": "/api_seasonal_weather/static/media/leaves1.d7c3af26.png"
+  },
+  {
+    "revision": "33ec9f01b1abe93a9375b8b162f3dc15",
+    "url": "/api_seasonal_weather/static/media/leaves2.33ec9f01.png"
+  },
+  {
+    "revision": "a50ff8533dfc80b0a17bab00702098c0",
+    "url": "/api_seasonal_weather/static/media/leaves3.a50ff853.png"
+  },
+  {
+    "revision": "7f6209d83239cdb2ff0d5f6b04e77fc5",
+    "url": "/api_seasonal_weather/static/media/leaves4.7f6209d8.png"
+  },
+  {
+    "revision": "297cd09c7ded0189a15947835c98e01f",
+    "url": "/api_seasonal_weather/static/media/plywood.297cd09c.jpg"
+  },
+  {
+    "revision": "4a8517c6d5e4f91b05fc5059b4082bd5",
+    "url": "/api_seasonal_weather/static/media/tree.4a8517c6.jpg"
+  }
+]);
